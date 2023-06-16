ef673217d4a2e152b5266a28aed42d68b619e6ad
Pawan Ramaswamy <pawanramaswamy@Pawans-MacBook-Pro.local>
2023-06-16 13:03:56 -0700

    made functional changes to some files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { useState } from "react"
+import BookCreate from "./components/BookCreate"
+import BookList from './components/BookList'
+
 function App() {
-    return <div>App</div>
+    const [books, setBooks] = useState([])
+
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !== id
+        })
+        setBooks(updatedBooks)
+    }
+
+    const createBook = (title) => {
+        const updatedBooks = [...books, {id: Math.round(Math.random() * 9999), title}]
+        setBooks(updatedBooks)
+    }
+
+    return (
+        <div className="app" >
+            <BookList books={books} onDelete={deleteBookById} />
+            <BookCreate onCreate={createBook} />
+        </div>
+    )
 }
 
 export default App
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,5 +1,29 @@
-function BookCreate (){
-    return <div>BookCreate</div>
+import { useState } from "react"
+function BookCreate ({onCreate}) {
+    const [title, setTitle] = useState('')
+
+    // Callback function to get the book title entered in the 'input' element
+    const handleChange = (event) => {
+        setTitle(event.target.value)
+    }
+
+    // Callback function to pass the value of the book title back to App.js
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        onCreate(title)
+        setTitle('')
+    }
+
+    return (
+        <div className="book-create" >
+            <h3>Add a Book</h3>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input className="input" value={title} onChange={handleChange} />
+                <button className="button" >Create</button>
+            </form>
+        </div>
+    )
 }
 
 export default BookCreate
